7d20986ecbc9583725d763a941a030f083951f71
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-17 19:56:55 +0300

    Instance Method Modifiers

--- classes.ts
+++ classes.ts
@@ -1,19 +1,26 @@
-class Vehicle {
-  drive(): void {
-    console.log("vrrum vrum");
-  }
+// public -> This method can be called any where, any time
 
-  honk(): void {
+// private -> This method can only be called bt other methods in this class
+
+// protected -> This method can be called by other methods in this class, or
+//              by other methods in child classes
+
+class Vehicle {
+  protected honk(): void {
     console.log("beep");
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
-car.drive();
-car.honk();
+car.startDrivingProcess();
