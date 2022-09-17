1a996e9f2d2e444f66ddac8b9a91017ab8afa69e
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-17 20:18:00 +0300

    Fields with Inheritance

--- classes.ts
+++ classes.ts
@@ -17,6 +17,10 @@ const vehicle = new Vehicle("orange");
 console.log(vehicle.color);
 
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
   private drive(): void {
     console.log("vroom");
   }
@@ -26,3 +30,6 @@ class Car extends Vehicle {
     this.honk();
   }
 }
+
+const car = new Car(4, "red");
+car.startDrivingProcess();
