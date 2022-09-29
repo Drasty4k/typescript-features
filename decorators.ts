620cb5a242e5cd4fea747bdff50559aa11b4f07d
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-29 19:43:59 +0300

    wrapped the pilot method with the logError Decorator to catch any error that it throws

--- decorators.ts
+++ decorators.ts
@@ -5,15 +5,22 @@ class Boat {
     return `This boats color is ${this.color}`;
   }
 
-  @testDecorator
+  @logError
   pilot(): void {
-    console.log("swish");
+    throw new Error();
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log("Target:", target);
-  console.log("Key", key);
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log("Oops, boat was sunk");
+    }
+  };
 }
 
-testDecorator(Boat.prototype, "pilot");
+new Boat().pilot();
