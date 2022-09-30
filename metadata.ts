8a4c6956c20f04eeb24bec289d93c1a5166a007d
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-30 18:06:09 +0300

    a look around some Practical Metadata (real project scenario)

--- metadata.ts
+++ metadata.ts
@@ -25,3 +25,30 @@ Reflect.defineMetadata("note", "hi there", plane, "color"); // attach metadata t
 const note2 = Reflect.getMetadata("note", plane, "color"); // retrieve metadata to the "color" key from the "plane" object
 
 console.log(note2);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@controller
+class Plane {
+  color: string = "red";
+
+  @get("/login")
+  fly(): void {
+    console.log("vrrrrrr");
+  }
+}
+
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata("path", path, target, key);
+  };
+}
+
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata("path", target.prototype, key);
+    const middleware = Reflect.getMetadata("path", target.prototype, key);
+
+    router.get(path, middleware, target.prototype[key]);
+  }
+}
